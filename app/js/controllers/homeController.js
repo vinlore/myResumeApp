2f3077e680645bd6a48110a78ded6ea04d9b5999
Vincent Lore <vincent_lore@hotmail.com>
2016-01-24 00:34:53 -0800

    UI changes and profile content
    Removed vancouver background, changed colours and margins

--- app/js/controllers/homeController.js
+++ app/js/controllers/homeController.js
@@ -3,6 +3,8 @@ angular.module('home.ctrl', [])
 
 	$scope.isCollapsed = true;
 
+	$scope.profile = "I'm Vincent, an upper level undergraduate student at UBC expecting to graduate this May with a double major in Computer Science and Biochemistry. As an upcoming new graduate, I'm currently actively seeking job opportunities in the tech sector."
+
 	$scope.languages = [
 		'HTML/CSS', 'Javascript', 'Java', 'SQL', 'Matlab', 'R', 'C++', 'PHP', 'Python' 
 	];
